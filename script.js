26db257731edb53faa0510006754c5f0478f8e89
git-user-ui <pratikpp2302@gmail.com>
2026-01-02 22:22:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-//your code here!
+const list = document.getElementById("infi-list");
 
+let count = 1;
+
+// Add items function
+function addItems(num) {
+  for (let i = 0; i < num; i++) {
+    const li = document.createElement("li");
+    li.textContent = `Item ${count++}`;
+    list.appendChild(li);
+  }
+}
+
+// Initial items
+addItems(10);
+
+// Infinite scroll
+list.addEventListener("scroll", () => {
+  // small threshold to avoid precision issues
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight - 5) {
+    addItems(2);
+  }
+});
